55d69e4e397b6fbae1bdc6ca2cbe5bf1a6edb5e4
FredyAlvarez01 <fredy.alvarez91@gmail.com>
2026-05-22 23:16:18 -0500

    3

--- notas.py
+++ notas.py
@@ -4,3 +4,4 @@
 ### test 4: Validación de Predicción de Paciente Sano
 ### test 5: Validación de Predicción de Paciente con Síntomas Leves pero Persistentes
 ## test 6: Validación de Predicción de Paciente con Síntomas Graves pero de Onset Reciente
+#### test 7: Validación de Predicción de Paciente con Síntomas Graves y Persistentes
