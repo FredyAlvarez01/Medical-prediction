c9683daec9fe5f7129e74b1ee66145f34f62997c
FredyAlvarez01 <fredy.alvarez91@gmail.com>
2026-05-22 22:38:32 -0500

    test 6

--- tests/notas.py
+++ tests/notas.py
@@ -2,3 +2,4 @@
 #### test 2: Validación de Predicción de Enfermedad Crónica
 ### test 3: Validación de Predicción de Enfermedad Leve
 ### test 4: Validación de Predicción de Paciente Sano
+### test 5: Validación de Predicción de Paciente con Síntomas Leves pero Persistentes
